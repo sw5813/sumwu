6f93a8aaa80313c92287737c946502f9bc016e59
Summer Wu <sw5813@gmail.com>
2014-10-30 10:40:45 -0400

    encrypt gmail data

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ personal site/project playground
 1. Start the virtual environment: source venv/bin/activate
 2. Start the server: python hello.py
 3. Check it out in a browser at: http://127.0.0.1:5000/
+	- Yale IMs site located at: http://127.0.0.1:5000/yaleims
 
 ### Pushing to production:
 1. After pushing the latest version to github, push it to heroku: git push heroku master
 2. Visit the site at: http://www.sumwu.me/
+	- Yale IMs site at: http://www.sumwu.me/yaleims
 
 ### To do:
 * Store old stats in mongodb database as backup in case connection goes haywire
--- controllers.py
+++ controllers.py
@@ -1,8 +1,16 @@
+from keyczar import keyczar
 import gspread
+
+def decrypt(ciphertext):
+    location = './keys'
+    crypter = keyczar.Crypter.Read(location)
+    plaintext = crypter.Decrypt(ciphertext)
+
+    return plaintext
 
 def overall():
 	# Login with your Google account
-	gc = gspread.login('summer.wu@yale.edu', '1sum$soc13')
+	gc = gspread.login(decrypt('AKEQNEBAIOxdrZIPdML6l0bu4Wt6jPKG88td--0KVA5xXluxBSPUlJuIwfvhZzuMKcf3GW1-KUmPyfcWizhQsXzMs8rGZ5zUiQ'), decrypt('AKEQNECl_fi8axz0d1i3t_Z-YmqRowycteVXl3bJGIshMiEcFcB7JaO-gcI6QSRtuvDuP91H2h3t'))
 
 	# Open the first worksheet
 	standings = gc.open_by_url("https://docs.google.com/a/yale.edu/spreadsheets/d/1fJ-bIkjFERyPApaGtUb3cvom7rO1bQYrig7Ly0Hy0jM/edit#gid=477165936").sheet1
