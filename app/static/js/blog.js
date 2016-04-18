a186653beeaec22841f1a7b6938ab97651c1a19d
Summer Wu <summer.wu82@gmail.com>
2016-04-18 13:21:54 +0200

    Add category bar for tech

--- app/static/js/blog.js
+++ app/static/js/blog.js
@@ -1,5 +1,18 @@
+// Show subscribe modal
 $(function() {
 	if (window.location.hash == "#subscribe") {
 		$('#subscribe').modal('show');
 	}
+});
+
+// Select current category
+$(window).load(function(){
+	var pathvalue = window.location.pathname;
+	var qvalue = window.location.search;
+	var full_url = pathvalue + qvalue;
+	$(".category").each(function(){
+		if ($(this).attr('href') == full_url) { 
+			$(this).addClass("active");
+		}
+	});
 });
