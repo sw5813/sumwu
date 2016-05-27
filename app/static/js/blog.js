08b353f3b8fd4af96499ebfac0693658737037d5
Summer Wu <summer.wu82@gmail.com>
2016-05-28 01:26:58 +0200

    Fix category highlighting for pagination by removing page numbers and article limits from path before checking

--- app/static/js/blog.js
+++ app/static/js/blog.js
@@ -10,8 +10,18 @@ $(window).load(function(){
 	var pathvalue = window.location.pathname;
 	var qvalue = window.location.search;
 	var full_url = pathvalue + qvalue;
+
+	// Handle pagination by removing page numbers and article limits from path before checking
+	var patharray = pathvalue.split('\/');
+	for (var i = 0; i < patharray.length; i++) {
+		if (!isNaN(parseFloat(patharray[i]))) {
+			patharray.splice(i, 1);
+		}
+	}
+	var newpath = patharray.join('\/');
+
 	$(".category").each(function(){
-		if ($(this).attr('href') == full_url) { 
+		if ($(this).attr('href') == newpath) { 
 			$(this).addClass("active_cat");
 		}
 	});
